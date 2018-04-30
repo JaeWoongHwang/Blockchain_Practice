8c4fa69a8025e622f0845c3cb2c8f91282cdd557
JaeWoongHwang <jwngh15@gmail.com>
2018-04-30 18:11:21 +0900

    Smart contracts of Ethereum

--- ethereum_voting_dapp/ebay_dapp/contracts/EcommerceStore.sol
+++ ethereum_voting_dapp/ebay_dapp/contracts/EcommerceStore.sol
@@ -23,6 +23,7 @@ contract EcommerceStore {
   uint totalBids;
   ProductStatus status;
   ProductCondition condition;
+  mapping (address => mapping (bytes32 => Bid)) bids;
  }
 
  function EcommerceStore() public {
@@ -45,4 +46,92 @@ contract EcommerceStore {
    return (product.id, product.name, product.category, product.imageLink, product.descLink, product.auctionStartTime,
        product.auctionEndTime, product.startPrice, product.status, product.condition);
  }
+
+ // Bid struct
+  struct Bid {
+   address bidder;
+   uint productId;
+   uint value;
+   bool revealed;
+ }
+
+  // Bid function
+  function bid(uint _productId, bytes32 _bid) payable public returns (bool) {
+    Product storage product = stores[productIdInStore[_productId]][_productId];
+    require (now >= product.auctionStartTime);
+    require (now <= product.auctionEndTime);
+    require (msg.value > product.startPrice);
+    require (product.bids[msg.sender][_bid].bidder == 0);
+    product.bids[msg.sender][_bid] = Bid(msg.sender, _productId, msg.value, false);
+    product.totalBids += 1;
+    return true;
+  }
+  // Reveal function
+  function revealBid(uint _productId, string _amount, string _secret) public {
+   Product storage product = stores[productIdInStore[_productId]][_productId];
+   require (now > product.auctionEndTime);
+   bytes32 sealedBid = sha3(_amount, _secret);
+
+   Bid memory bidInfo = product.bids[msg.sender][sealedBid];
+   require (bidInfo.bidder > 0);
+   require (bidInfo.revealed == false);
+
+   uint refund;
+
+   uint amount = stringToUint(_amount);
+
+   if(bidInfo.value < amount) {
+    // They didn't send enough amount, they lost
+    refund = bidInfo.value;
+   } else {
+    // If first to reveal set as highest bidder
+    if (address(product.highestBidder) == 0) {
+     product.highestBidder = msg.sender;
+     product.highestBid = amount;
+     product.secondHighestBid = product.startPrice;
+     refund = bidInfo.value - amount;
+    } else {
+     if (amount > product.highestBid) {
+      product.secondHighestBid = product.highestBid;
+      product.highestBidder.transfer(product.highestBid);
+      product.highestBidder = msg.sender;
+      product.highestBid = amount;
+      refund = bidInfo.value - amount;
+     } else if (amount > product.secondHighestBid) {
+      product.secondHighestBid = amount;
+      refund = amount;
+     } else {
+      refund = amount;
+     }
+    }
+   }
+   product.bids[msg.sender][sealedBid].revealed = true;
+
+   if (refund > 0) {
+    msg.sender.transfer(refund);
+   }
+  }
+
+  // Getter & Helper functions
+  function highestBidderInfo(uint _productId) view public returns (address, uint, uint) {
+  Product memory product = stores[productIdInStore[_productId]][_productId];
+  return (product.highestBidder, product.highestBid, product.secondHighestBid);
+  }
+
+  function totalBids(uint _productId) view public returns (uint) {
+    Product memory product = stores[productIdInStore[_productId]][_productId];
+    return product.totalBids;
+  }
+
+  function stringToUint(string s) pure private returns (uint) {
+    bytes memory b = bytes(s);
+    uint result = 0;
+    for (uint i = 0; i < b.length; i++) {
+      if (b[i] >= 48 && b[i] <= 57) {
+        result = result * 10 + (uint(b[i]) - 48);
+      }
+    }
+    return result;
+  }
+
 }
--- ethereum_voting_dapp/ebay_dapp/truffle.js
+++ ethereum_voting_dapp/ebay_dapp/truffle.js
@@ -2,11 +2,11 @@
 require('babel-register')
 
 module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '*' // Match any network id
-    }
+ networks: {
+  development: {
+   host: 'localhost',
+   port: 8545,
+   network_id: '*'
   }
+ }
 }
